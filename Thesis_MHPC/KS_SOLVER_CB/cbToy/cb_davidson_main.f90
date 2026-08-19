9e0e767b49fa473503983087fa3b9b794a6fbff4
Whatever Name <whatever@whatever>
2026-08-19 10:12:08 +0200

    davidson main

--- Thesis_MHPC/KS_SOLVER_CB/cbToy/cb_davidson_main.f90
+++ Thesis_MHPC/KS_SOLVER_CB/cbToy/cb_davidson_main.f90
@@ -1,4 +1,4 @@
-   program cb_davidson_main
+program cb_davidson_main
 
 ! global variables
    USE cb_module
@@ -7,20 +7,30 @@
    use mp_world,             ONLY : world_comm
    use mp_bands,             ONLY : intra_bgrp_comm, inter_bgrp_comm
 #endif
-
+   use mytime,               only: t0cpu, clock_label, nclock, clock_thread, clock_cuda_stream, clock_locker, &
+                                   fftw_locker, cegterg_locker            
+   use omp_lib,               only: omp_get_thread_num, omp_set_lock, omp_unset_lock, omp_init_lock
+#if defined(__CUDA)
+   use openacc,               only: acc_get_cuda_stream
+   use laxlib_cusolver_handles, ONLY : initialize_cusolver_handles, initialize_laxlib_cuda_stream
+#endif
+   !!use nvpl_lapack,         only: nvpl_lapack_set_num_threads
    implicit none
    !
-   include 'laxlib.fh'
+   !include 'laxlib.fh'
    !
 ! local variables (used in the call to cegterg )
    logical, parameter :: gamma_only = .false. ! general k-point version
-   complex(DP), allocatable :: evc(:,:)
-   real(dp), allocatable :: eig(:)
+   complex(DP), allocatable :: evc(:,:), evc_batched(:,:,:) 
+   real(dp), allocatable :: eig(:), eig_batched(:,:) 
    integer, parameter :: npol=1
+   integer :: dummy
    integer :: notcnv, dav_iter, nhpsi
+   integer, allocatable :: notcnv_batched(:), dav_iter_batched(:), nhpsi_batched(:)
    logical :: overlap = .false. , lrot =.false.
 ! additional local variables
    real(dp) :: ref=0.d0
+   integer :: i_batch, ik
 #if defined(__MPI)
 ! local paralelization variables
    integer :: ndiag     ! input value of processors in the diagonalization group
@@ -28,7 +38,8 @@
                                                     ! band group or at the parallelization level above.
 #endif
 !------------------------------------------------------------------------
-   external cb_h_psi, cb_s_psi, cb_g_psi
+   external my_h_psi_batched, cb_h_psi, cb_s_psi_batched, cb_g_psi_batched
+   external cb_s_psi, cb_g_psi
 !  subroutine cb_h_psi(npwx,npw,nvec,psi,hpsi)  computes H*psi
 !  subroutine cb_s_psi(npwx,npw,nvec,psi,spsi)  computes S*psi (if needed)
 !  subroutine cb_g_psi(npwx,npw,nvec,psi,eig)   computes G*psi -> psi
@@ -47,47 +58,105 @@
 !--------------------------------------------------------------------------------------------------------------!
 #endif
 
-   call init_clocks(.true.)
+   ! 
+
    call input(gamma_only)
+   print *, 'Running cb_davidson_main with the following parameters:'
+   call initialize_cusolver_handles(nk_batches) 
+   print *, 'nk_batches = ', nk_batches
+   !$omp parallel num_threads(nk_batches) default(shared)  shared(t0cpu, nclock, clock_label) 
+   call init_clocks(.true.)
+   call omp_init_lock(cegterg_locker) 
+   call omp_init_lock(fftw_locker) 
+#if defined(__CUDA)
+   !$omp do 
+   do i_batch = 1, nk_batches
+      clock_thread = i_batch
+      clock_cuda_stream = acc_get_cuda_stream(clock_thread)
+      call initialize_laxlib_cuda_stream(clock_cuda_stream, clock_thread)
+      print '("Initialized default stream in thread ",2I5,I24)', omp_get_thread_num(), clock_thread, clock_cuda_stream
+   end do 
+#endif
+   !$omp end parallel
+
+
+ 
+
+   allocate(npw_batched(nk_batches)) 
+   allocate(notcnv_batched(nk_batches), dav_iter_batched(nk_batches), nhpsi_batched(nk_batches))
    call ggen(gamma_only)
    call set_cb_potential
-
    if (use_overlap) write(*,*) '** TEST:  CB hamiltonian modified so as to need an overlap matrix **'
    overlap = use_overlap
 
-   do current_k=1,nks
-     call init_k
-     allocate( evc(npwx,nbnd), eig(nbnd) )
-     call init_random_wfcs(npw,npwx,nbnd,evc)
+   allocate( evc_batched(npwx,nbnd,nk_batches), eig_batched(nbnd,nk_batches) )
+   allocate( fft_array_batched(dfft%nnr, nk_batches), aux_batched(dfft%nnr, nk_batches) )
+   allocate (evc(npwx, nbnd), eig(nbnd)) 
+   !$acc enter data create(evc_batched, eig_batched, fft_array_batched, aux_batched)
 
+   do ik =1,nks, nk_batches
      call start_clock('davidson')
+     !$omp parallel num_threads(nk_batches) default(shared) private(i_batch) shared(t0cpu, nclock, clock_label) 
+     !$omp do
+     do i_batch = 1, min(nk_batches, nks - ik +1) 
+       !clock thread is declared threadprivate in the module 
+       clock_thread = i_batch 
+
+     #if defined(__CUDA) 
+   ! Reset stream for this thread in this parallel region
+       clock_cuda_stream = acc_get_cuda_stream(clock_thread)
+     #endif 
+
+       print '("First loop, batch ",3I5)', i_batch, clock_thread, omp_get_thread_num()  
+       current_k = ik + i_batch -1   
+
+       call init_k(current_k, i_batch) 
+       !$acc update device(igk_batched(:,i_batch)) async(clock_thread)
+       call init_random_wfcs(npw_batched(i_batch), npwx, nbnd, evc_batched(1,1,i_batch),i_batch)  
+       !$acc update device(evc_batched(:,:,i_batch)) async(clock_thread)
+       !$acc host_data use_device(eig_batched(1,i_batch))
+#if defined(__INTERCALATE_CEGTERG)
+       call omp_set_lock(cegterg_locker) 
+#endif
+       call cegterg( my_h_psi_batched, cb_s_psi_batched, overlap, cb_g_psi_batched, &
+                      npw_batched(i_batch), npwx, nbnd, nbndx, npol, evc_batched(1,1,i_batch), ethr, &
+                      eig_batched(1,i_batch), btype, notcnv_batched(i_batch), lrot, dav_iter_batched(i_batch), & 
+                      nhpsi_batched(i_batch), i_batch, nks ) !!M.Iovine - added nks as argument of the subroutine
+       !$acc end host_data  
+#if defined(__INTERCALATE_CEGTERG)
+      call omp_unset(cegterg_locker)
+#endif
+     end do 
+     !$omp end parallel 
+     call stop_clock('davidson') 
+     ! !$omp barrier   !Remove
+     !$acc wait  
+     !$acc update self(eig_batched) 
+     ! Second loop: Process batches sequentially
+     do i_batch =1, min(nk_batches, nks - ik +1 )  
+        print '("Second loop, batch ",I5)', i_batch 
+        current_k = ik + i_batch -1   
+        
 !--- THIS IS THE RELEVANT CALL TO THE ROUTINE IN KS_Solvers/Davidson ------------------------------------------!
-#if defined(__MPI)
-     write (stdout,*) 'ndiag', ndiag
-     if (ndiag == 1) then
-#endif
-        call cegterg( cb_h_psi, cb_s_psi, overlap, cb_g_psi, &
-                      npw, npwx, nbnd, nbndx, npol, evc, ethr, &
-                      eig, btype, notcnv, lrot, dav_iter, nhpsi )
-#if defined(__MPI)
-     else
-        call pcegterg(cb_h_psi, cb_s_psi, overlap, cb_g_psi, &
-                      npw, npwx, nbnd, nbndx, npol, evc, ethr, &
-                      eig, btype, notcnv, lrot, dav_iter, nhpsi )
-     end if
-#endif
 !--------------------------------------------------------------------------------------------------------------!
 
-     call stop_clock('davidson')
-     deallocate( evc )
-
-     if (energy_shift .and. current_k==1) ref=eig(4*ncell**3)
-     call write_bands(eig,ref)
-     write (stdout,*) 'dav_iter, nhpsi, notcnv, ethr ', dav_iter, nhpsi, notcnv, ethr
-
-     deallocate( eig )
+        
+        
+        if (energy_shift .and. current_k==1) ref=eig_batched(4*ncell**3,i_batch)
+     
+        call write_bands(eig_batched(1,i_batch),ref)
+        write (stdout,*) 'batch', i_batch, 'dav_iter, nhpsi, notcnv, ethr ', &
+                         dav_iter_batched(i_batch), nhpsi_batched(i_batch), notcnv_batched(i_batch), ethr !Fixed
+     end do 
    end do
-
+   
+   !$acc exit data delete(evc, eig, fft_array_batched, aux_batched)
+   !$acc exit data delete(dfft, dfft%nl, dfft%nnr, igk, vloc) 
+   deallocate( eig )
+   deallocate( evc )
+   deallocate( evc_batched, eig_batched )
+   deallocate( fft_array_batched, aux_batched )
+   deallocate( notcnv_batched, dav_iter_batched, nhpsi_batched )
    call print_clock('davidson')
 
    call print_clock( 'cegterg' )
